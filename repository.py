93b1f4d7f64e507a5fbed0e99146fb852b6a5f0d
vuvuzella <j.tabacii@gmail.com>
2022-12-29 09:24:42 +1100

    added tests that uses repository

--- repository.py
+++ repository.py
@@ -23,8 +23,24 @@ class SqlAlchemyRepository(AbstractRepository):
     def add(self, batch: Batch):
         self._session.add(batch)
 
-    def get(self, reference) -> Batch:
+    def get(self, reference: str) -> Batch:
         return self._session.query(Batch).filter_by(reference=reference).one()
 
     def list(self) -> List[Batch]:
         return self._session.query(Batch).all()
+
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches: List[Batch]) -> None:
+        super().__init__()
+        self._batches = batches
+
+    # TODO: no uniqueness validation or check!
+    def add(self, batch) -> None:
+        self._batches.append(batch)
+
+    def get(self, reference: str) -> Batch:
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self) -> List[Batch]:
+        return self._batches
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,8 +1,55 @@
-from models import Batch
+from models import Batch, OrderLine
 
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 from repository import SqlAlchemyRepository
+
+
+def insert_order_lines(session: Session):
+    session.execute(
+        text(
+            """
+        INSERT INTO order_lines (orderid, sku, qty) VALUES
+            ('order1', 'GENERIC-SOFA', 12);
+        """
+        )
+    )
+
+    [[order_line_id]] = session.execute(
+        text(
+            """
+        SELECT id FROM order_lines
+        WHERE orderid=:orderid AND sku=:sku
+        """
+        ),
+        {"orderid": "order1", "sku": "GENERIC-SOFA"},
+    )
+    return order_line_id
+
+
+# TODO: implement batch insertion
+def insert_batch(session: Session, batch_reference: str) -> int:
+    ...
+
+
+# TODO: implement insert allocation
+# This might require modifying tables and mappings!
+def insert_allocation(session: Session, order_line_id: int, batch_id: int):
+    ...
+
+
+def test_repository_can_retrieve_a_batch_with_allocations(session: Session):
+    order_line_id = insert_order_lines(session)
+    batch_one_id = insert_batch(session, "batch-one")
+    batch_two_id = insert_batch(session, "batch-two")
+    insert_allocation(session, order_line_id, batch_one_id)  # TODO: implement this one
+    repo = SqlAlchemyRepository(session)
+    retrieved = repo.get("batch-one")
+    expected = Batch("batch-one", "GENERIC-SOFA", 100, eta=None)
+    assert retrieved == expected
+    assert retrieved.name == expected.name
+    assert retrieved._purchased_quantity == expected._purchased_quantity
+    assert retrieved._allocations == {OrderLine("order-one", "GENEROC-SOFA", 12)}
 
 
 def test_repository_can_save_a_batch(session: Session):
