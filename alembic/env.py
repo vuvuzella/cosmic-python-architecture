497f523be9e1eca2e97e424043c6481082ad5edd
vuvuzella <j.tabacii@gmail.com>
2023-12-02 18:34:01 +1100

    fixed alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -66,7 +66,12 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            compare_server_default=True,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
